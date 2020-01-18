835af33bac790250a74d0cda6636dcb95a34bdd3
Dyla n ra y <technicalpro@securemedia.ch>
2020-01-18 17:48:58 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,9 @@
-def hello_t
-
+["Tim", "Tom", "Jim"].each do |name|
+  puts "Hi, #{name}"
 end
 
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+end
 # call your method here!
-
